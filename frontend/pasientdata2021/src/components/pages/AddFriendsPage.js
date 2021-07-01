a13edf1b07a0451855735fea86365a623a388dd0
Awalle Mohamed <awallemo_98@hotmail.com>
2021-07-01 13:34:16 +0200

    Begynt med litt implementering av logikk i addfriendspage

--- frontend/pasientdata2021/src/components/pages/AddFriendsPage.js
+++ frontend/pasientdata2021/src/components/pages/AddFriendsPage.js
@@ -4,19 +4,37 @@ import LoginButton from "../buttons/LoginButton";
 
 import WhiteHeaderWrapper from "../boxes/WhiteHeaderWrapper";
 import GreenBoxRoundedCorner from "../boxes/GreenBoxRoundedCorner";
+import { useState } from "react";
+import axios from "axios";
 
 const LoginButtonTheme = {
     marginTop: "40px"
 }
 
 
+
+
 function AddFriendsPage() {
+
+  const [requestResult, setRequestResult] = useState({});
+  const [userInput, setUserInput] = useState("");
+  console.log(userInput)
+  
+
+  function sendData(friendName){
+      console.log("SEND REQUEST WITH USERNAME: ".concat(friendName))
+      
+      axios.post('/Friend/AddFriend/',{friendName})
+          .then(response => setRequestResult(response.data));
+  }
+  
+    
     return (
       <>
       <WhiteHeaderWrapper title="Legg til venner" />
       <GreenBoxRoundedCorner>
-        <UserInputField placeholder="Brukernavn" />
-        <LoginButton classname theme={LoginButtonTheme}>Legg til</LoginButton>
+        <UserInputField placeholder="Brukernavn" onChange={e=>setUserInput(e.target.value)}/>
+        <LoginButton classname theme={LoginButtonTheme} onClick={()=>sendData(userInput)}>Legg til</LoginButton>
       </GreenBoxRoundedCorner>
       </>
     )
