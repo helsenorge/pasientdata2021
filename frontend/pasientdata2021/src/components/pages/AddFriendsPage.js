aa69a43283681ee21682f6603127c2321962d883
Awalle Mohamed <awallemo_98@hotmail.com>
2021-07-07 08:57:28 +0200

    Navneforslag og popup-tekst når du legger til en

--- frontend/pasientdata2021/src/components/pages/AddFriendsPage.js
+++ frontend/pasientdata2021/src/components/pages/AddFriendsPage.js
@@ -6,50 +6,93 @@ import WhiteHeaderWrapper from "../boxes/WhiteHeaderWrapper";
 import GreenBoxRoundedCorner from "../boxes/GreenBoxRoundedCorner";
 import { useState } from "react";
 import axios from "axios";
+import {Autocomplete} from '@material-ui/lab';
+import {TextField} from '@material-ui/core';
+import styled from "styled-components";
 
 const LoginButtonTheme = {
     marginTop: "40px"
 }
 
 
+const BottomText = styled.div`
+    text-align: center;
+    font-size: 20px;
+    color: #6C757D;
+`
 
 
 function AddFriendsPage() {
 
   
   //Søker etter brukere
-  const [requestUsers, setRequestUser] = useState({})
+  const [requestUsers, setRequestUser] = useState([])
   const [requestResult, setRequestResult] = useState({});
+  const [acceptMessage, setAcceptMessage] = useState(false)
+  const [errorMessage, setErrorMessage] = useState(false)
   
+  const [selectedUser, setSelectedUser] = useState();
     
   function searchResult(key){
     axios.get('user/search/'+key)
     .then(response => setRequestUser(response.data))
-    console.log("FAEN")
+    console.log(requestUsers)
     
   }
   
   console.log(requestUsers)
   //Legger til valgte brukerer som venn ved å bruke /Addfriend routen til backenden
   function sendFriendRequest(friendName){
-    console.log("SEND REQUEST WITH USERNAME: "+ (friendName))
+    console.log("selectedUser: "+ (friendName.username))
+
+    //Må tømme set
     
-    axios.post('/Friend/AddFriend/'+ friendName)
-        .then(response => setRequestResult(response.data))
+    axios.post('/Friend/AddFriend/'+ friendName.id)
+        .then(response => {          
+            setAcceptMessage(true)
+            setErrorMessage(false)
+        })
         .catch(function (error) {
           // handle error
           console.log(error.response.data);
+          setErrorMessage(true)
+          setAcceptMessage(false)
         })
 }
-  
+
+
     
     return (
       <>
       <WhiteHeaderWrapper title="Legg til venner" />
       <GreenBoxRoundedCorner>
-        <UserInputField placeholder="Brukernavn" onChange={e=>searchResult(e.target.value)}/>
+       
+          <Autocomplete
+          id="combo-box-demo"
+          options={requestUsers}
+          getOptionLabel={(option) => option.username}
+          onChange={(event, value)=>setSelectedUser(value)}
+          getOptionSelected = {(option, value) => option.username === value.username}
+          style={{ width: 370 }}
+          renderInput={(params) => <TextField {...params} onChange={e=>searchResult(e.target.value)} label="Brukernavn" variant="outlined" />}
+        />
 
-        <LoginButton classname theme={LoginButtonTheme} onClick={()=>sendFriendRequest(requestUsers[0]["id"])}>Legg til</LoginButton>
+        {errorMessage ? 
+          <BottomText>
+          Kunne ikke sende venneforspørselen. Du har allerede lagt til brukeren eller skrevet feil brukernavn.
+          </BottomText> : ""
+          }
+        {acceptMessage ?
+        <BottomText>
+          venneforspørselen ble sendt!
+        </BottomText> : ""
+
+        }
+
+
+    
+
+        <LoginButton classname theme={LoginButtonTheme} onClick={()=>sendFriendRequest(selectedUser)}>Legg til</LoginButton>
       </GreenBoxRoundedCorner>
       </>
     )
